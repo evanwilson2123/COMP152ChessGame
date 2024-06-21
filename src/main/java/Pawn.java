1aa51ffcd862340e6d24aa420c799a630614c56f
Evan Wilson <evanmwilson2123@gmail.com>
2024-06-21 18:19:17 -0400

    Cleaned up comments

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -12,56 +12,6 @@ public class Pawn extends Piece{
 
     }
     // DONE
-//    public boolean canMove(int startx, int starty, int endx, int endy) {
-//        int direction;
-//        if (isWhite) {
-//            direction = -1;
-//        } else {
-//            direction = 1;
-//        }
-//
-//        // Normal move
-//        if (starty == endy) {
-//            if (startx + direction == endx) {
-//                if (Chess.position[endx][endy] == null){
-//                    return true;
-//                } else if (getColor() == Chess.position[endx][endy].getColor()) {
-//                    return false;
-//                } else if (getColor() != Chess.position[endx][endy].getColor()){
-//                    return true;
-//                }
-//                return false;
-//            }
-//            // First move can be two steps
-//            if (getMoveCount() == 0 && startx + 2 * direction == endx &&
-//                    Chess.position[endx][endy] == null && Chess.position[endx][starty + direction] == null) {
-//                if (Chess.position[endx][endy] == null){
-//                    return true;
-//                } else if (getColor() == Chess.position[endx][endy].getColor()) {
-//                    return false;
-//                } else if (getColor() != Chess.position[endx][endy].getColor()){
-//                    return true;
-//                }
-//                return false;
-//            }
-//        }
-//
-//        // Capture move
-//        if (Math.abs(starty - endy) == 1 && startx + direction == endx &&
-//                Chess.position[endx][endy] != null && Chess.position[endx][endy].isWhite != isWhite) {
-//            if (Chess.position[endx][endy] == null){
-//                return true;
-//            } else if (getColor() == Chess.position[endx][endy].getColor()) {
-//                return false;
-//            } else if (getColor() != Chess.position[endx][endy].getColor()){
-//                return true;
-//            }
-//            return false;
-//        }
-//
-//        return false;
-//    }
-    // DONE
     public boolean canMove(int startx, int starty, int endx, int endy) {
         int direction;
         if (isWhite) {
